2d719e42b74f2703e8eeb1ef3b0f26133deb5129
tsakib2000 <isakib49@gmail.com>
2025-01-24 23:06:49 +0600

    edite private route

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -6,18 +6,20 @@ import { redirect } from "next/navigation";
 export default async function Protected() {
     const { isAuthenticated } = getKindeServerSession();
     const user= await isAuthenticated() 
-    console.log(user);
-if(user == false) {
-    return redirect('/api/auth/login') 
-}
+    console.log(isAuthenticated);
+if(user) 
 
-  return (
+  {
+    return (
 
-    <div className="flex flex-col justify-center items-center h-screen">
-     <h1 className="text-3xl font-bold">Welcome to your profile</h1>
-    
- 
-            </div>
-    
-        ) 
+        <div className="flex flex-col justify-center items-center h-screen">
+         <h1 className="text-3xl font-bold">Welcome to your profile</h1>
+        
+     
+                </div>
+        
+            ) 
+  }else{
+    redirect('/api/auth/login')
+  }
 }
