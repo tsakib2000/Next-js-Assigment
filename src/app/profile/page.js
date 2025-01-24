7bcc30a84e476f40140f6cb88aa90afdc97e5dea
tsakib2000 <isakib49@gmail.com>
2025-01-24 21:57:53 +0600

    edited profile  route

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -4,19 +4,15 @@ import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from "next/navigation";
 
 export default async function Protected() {
-  const { getUser,isAuthenticated } = getKindeServerSession();
+  const { isAuthenticated } = getKindeServerSession();
 
-const {user}=await getUser();
+
   return (await isAuthenticated()) ?(
 
     <div className="flex flex-col justify-center items-center h-screen">
      <h1 className="text-3xl font-bold">Welcome to your profile</h1>
     
-     <div className="border p-4 mt-5">
-        <h3>Name: {user?.given_name} {user?.family_name}</h3>
-        <h4>Email: {user?.email}</h4>
-         
-     </div>
+ 
             </div>
     
         ) : redirect('/api/auth/login')
