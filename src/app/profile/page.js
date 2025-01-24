ee1e37489d68da265c13f132053a11f6696a8579
tsakib2000 <isakib49@gmail.com>
2025-01-24 22:45:35 +0600

    edite private route

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -4,9 +4,12 @@ import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from "next/navigation";
 
 export default async function Protected() {
-    const { getUser } = getKindeServerSession();
-    const user = await getUser();
-if(!user) return redirect('/api/auth/login')
+    const { isAuthenticated } = getKindeServerSession();
+    const user= await isAuthenticated() 
+    console.log(user);
+if(!user){
+    return redirect('/api/auth/login') 
+}
 
   return (
 
