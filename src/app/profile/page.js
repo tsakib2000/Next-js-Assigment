9c7c394ee49ba96ddb174fc15fb44e45f54862a9
tsakib2000 <isakib49@gmail.com>
2025-01-24 21:23:41 +0600

    edited profile  route

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,31 +1,23 @@
-
+import { LoginLink } from "@kinde-oss/kinde-auth-nextjs/components";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { redirect } from "next/navigation";
 
+export default async function Protected() {
+  const { isAuthenticated } = getKindeServerSession();
 
+  return (await isAuthenticated()) ? (
+    <div className="flex flex-col justify-center items-center h-screen">
+      <h1 className="text-3xl font-bold">Welcome to your profile</h1>
 
-
-
-const about =  async() => {
-
-    const { isAuthenticated} = getKindeServerSession();
-    if(!(await isAuthenticated())){
-        redirect('/api/auth/login')
-    }
-
-
-    return (
-
-<div className="flex flex-col justify-center items-center h-screen">
- <h1 className="text-3xl font-bold">Welcome to your profile</h1>
-
- <div className="border p-4 mt-5">
-
-     
- </div>
-        </div>
-
-    );
-};
-
-export default about;
+      <div className="border p-4 mt-5">
+        <h3>
+          Name: {user?.given_name} {user?.family_name}
+        </h3>
+        <h4>Email: {user?.email}</h4>
+      </div>
+    </div>
+  ) : (
+    <div>
+      This page is protected, please <LoginLink>Login</LoginLink> to view it
+    </div>
+  );
+}
